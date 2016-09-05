3bcadc9e24097b3c37d1494b3d0b76102b3534ba
quantumke <nguruben@gmail.com>
2016-09-05 21:44:47 +0300

    ui login class

--- login_interface.py
+++ login_interface.py
@@ -32,3 +32,42 @@ class GuiInit(tkinter.Tk):
             frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame(LoginPage)
+          def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+class LoginPage(ttk.Frame):
+
+    def __init__(self, parent, controller):
+        ttk.Frame.__init__(self,parent)
+        self.login_gui()
+    def login_gui(self):
+        # self.root.title('WELCOME')
+        self.grid(column=0, row=0, sticky='nsew')
+        self.username = ttk.Entry(self, width=5)
+        self.username.grid(column=1, row=2)
+        self.password = ttk.Entry(self, width=5)
+        self.password.grid(column=3, row=2)
+        self.submit_button = ttk.Button(self, text='Login', command=self.login)
+        self.submit_button.grid(column=0, row=3, columnspan=4)
+        self.status = ttk.LabelFrame(self, text='Status',
+									 height=100)
+        self.status.grid(column=0, row=4, columnspan=4, sticky='nesw')
+
+        self.status_label = ttk.Label(self.status, text='')
+        self.status_label.grid(column=0, row=0)
+
+        ttk.Label(self, text='Login').grid(column=0, row=0,
+										   columnspan=4)
+        ttk.Label(self, text='Username').grid(column=0, row=2,
+											  sticky='w')
+        ttk.Label(self, text='Password').grid(column=2, row=2,
+											  sticky='w')
+
+        ttk.Separator(self, orient='horizontal').grid(column=0,
+													  row=1, columnspan=4, sticky='ew')
+
+        for child in self.winfo_children():
+            child.grid_configure(padx=5, pady=5)
