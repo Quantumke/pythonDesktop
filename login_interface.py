c218c8d8ff3a56d4083c50a88c986b2fa7965f39
quantumke <nguruben@gmail.com>
2016-09-05 21:44:12 +0300

    login frame init

--- login_interface.py
+++ login_interface.py
@@ -9,3 +9,26 @@ import psycopg2
 
 LARGE_FONT= ("Verdana", 12)
 
+class GuiInit(tkinter.Tk):
+
+    def __init__(self, *args, **kwargs):
+
+        tkinter.Tk.__init__(self, *args, **kwargs)
+        container = tkinter.Frame(self)
+
+        container.pack(side="top", fill="both", expand = True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (LoginPage, PageOne, PageTwo):
+
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(LoginPage)
